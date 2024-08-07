c72bc2649da9c5cacf22dabbad81044a6741431a
Alex Tykoun <alextykoun@gmail.com>
2024-08-08 00:11:59 +0300

    Initial commit. Abstracts for Entities added to Core

---
+++ src/MoreMath.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace MoreMath.Core.Abstracts;
+
+public abstract class BaseEntity<TKey> where TKey : struct, IEquatable<TKey>
+{
+    public TKey Id { get; set; }
+}
---
+++ src/MoreMath.Core/Abstracts/EntityWithDates.cs
@@ -0,0 +1,7 @@
+﻿namespace MoreMath.Core.Abstracts;
+
+public abstract class EntityWithDates<TKey> : BaseEntity<TKey> where TKey : struct, IEquatable<TKey>
+{
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime Modified { get; set; } = DateTime.UtcNow;
+}
