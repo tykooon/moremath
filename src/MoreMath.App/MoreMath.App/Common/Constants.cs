2ee84f00fc9958110849f0d73f55035c8ddb4a5b
Alex Tykoun <alextykoun@gmail.com>
2024-09-17 02:55:49 +0300

    Drat-version of Home-Page is published. CSS, Root structure added

--- src/MoreMath.App/MoreMath.App/Common/Constants.cs
+++ src/MoreMath.App/MoreMath.App/Common/Constants.cs
@@ -12,5 +12,10 @@ public static class Constants
         public const string Blog = "articles/blog";
         public const string Team = "about/team";
         public const string Contacts = "about/contacts";
+        public const string JoinTeam = "about/team/join";
+        public const string TestLesson = "lessons/letstry";
+        public const string AlexTykoun = "about/team/alextykoun";
+        public const string OlgaPindrik = "about/team/olgapindrik";
+
     }
 }
