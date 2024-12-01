d607320ed11d106e16fee1227146712047be74c4
Alex Tykoun <alextykoun@gmail.com>
2024-12-02 00:57:57 +0200

    Issue with DataGrid Skip Reset solved; Issue with Author page redundant mjax solved;

---
+++ src/MoreMath.App/MoreMath.App/Common/HtmlHelpers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MoreMath.App.Common;
+
+public static class HtmlHelpers
+{
+    public static MarkupString ToMarkupWithProcessing(this string text)
+    {
+        text = text.Replace(" --", "&nbsp;&ndash;");
+        text = text.Replace(" ---", "&nbsp;&mdash;");
+        text = text.Replace("\\\\", "</p><p>");
+        text = text.Replace("\\-", "&shy;");
+        return new MarkupString(text);
+    }
+}
